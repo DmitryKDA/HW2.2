256a1cdbf4cb9e6b3ba3c05405351de6b778e73a
Dmitry <siliconworks.dm@gmail.com>
2021-01-04 18:49:02 +0300

    Initial Commit

---
+++ HW2.2ChangeColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.2ChangeColor
+//
+//  Created by Дмитрий Крутько on 4.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
